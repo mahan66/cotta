1fd3690282f08429bbf8be35175b31af328369a2
Mahmood Karimian <mahmood.karimian@gmail.com>
2022-06-20 23:06:52 +0200

    test sh files

--- cifar/run_cifar10.sh
+++ cifar/run_cifar10.sh
@@ -2,9 +2,9 @@
 export PYTHONPATH= 
 conda deactivate
 conda activate cotta 
-CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/source.yaml
-CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/norm.yaml
-CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/tent.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/source.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/norm.yaml
+#CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/tent.yaml
 CUDA_VISIBLE_DEVICES=0 python cifar10c.py --cfg cfgs/cifar10/cotta.yaml
 
 
